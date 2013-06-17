2b39fa3a032903d5af5bbb67d5ded3cb2df863c8
Joseph <Joe@Joseph-Milsoms-MacBook-Pro.local>
2013-06-17 23:11:30 +1200

    map
    Messing around with map getting locations

--- src/forage/MapServlet.java
+++ src/forage/MapServlet.java
@@ -10,36 +10,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @SuppressWarnings("serial")
 public class MapServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		String maps = "";
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		//String maps = "";
+		//BufferedReader reader;
+		Entity loc;
+		Key location = KeyFactory.createKey("Location", "Location");
+		try{
+		loc = datastore.get(location);
+		}catch(Exception e){
+			resp.setContentType("text/html");
+			resp.getWriter().println(
+					"<!DOCTYPE html><html><body>Entity Not Found</body></html>");
+		}
+		/*
 		try{
 			resp.setContentType("text/html");
 			ServletContext  s= this.getServletContext();
 			String path = s.getRealPath("mapcode.txt");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
+		 reader = new BufferedReader(new InputStreamReader(
 				new FileInputStream(path), "UTF8"));
-		req.getAttribute("type");
-		/*Query itemQuery = new Query("FoodItem")
-		.setAncestor(pKeys.get(0))
-		.addFilter(Entity.KEY_RESERVED_PROPERTY,
-				Query.FilterOperator.GREATER_THAN, pKeys.get(0));*/
-		//Joe is awesome
+		//req.getAttribute("type");
+
 		maps = reader.readLine();
 		//get locations here format placeMarker(lon, lat, title, info) (dont change info) Also title is formatted as html
 		maps += reader.readLine();
 		
 		resp.getWriter().println(maps);
-		reader.close();
 		}catch(IOException e){
 			resp.setContentType("text/html");
 			resp.getWriter().println(
 					"<!DOCTYPE html><html><body>Error Reading Text</body></html>");
 		}
+		finally{
+			reader.close();
+		}*/
 	}
 }
