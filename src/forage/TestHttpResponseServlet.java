ad4be100f91dba859e5d49617387daa5f9ae2fd2
Joseph <Joe@Joseph-Milsoms-MacBook-Pro.local>
2013-06-22 16:01:45 +1200

    adding forms to maps
    adding forms to maps + new icons

---
+++ src/forage/TestHttpResponseServlet.java
@@ -0,0 +1,16 @@
+package forage;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestHttpResponseServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
